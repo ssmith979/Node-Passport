99c54b7025585e4f35833457a85b460aaab04f77
Susanna Smith <50603908+ssmith979@users.noreply.github.com>
2020-04-07 20:56:00 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var path = require('path');
+var expressValidator = require('express-validator');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy
+var bodyParser = require('body-parser');
+var flash = require('connect-flash');
+
+var  routes = require('./routes/index');
+var  users = require('./routes/users');
+
+var app = express();
+
+// View Engine
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/css', express.static(__dirname+ '/node_modules/bootstrap/dist/css'))
+
+// BodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Express Session Middleware (github has this code)
+app.use(session({
+    secret: 'secret',
+    saveUnitialized: true,
+    resave: true
+}));
+
+
+// Passpor Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+  
+      while(namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param : formParam,
+        msg   : msg,
+        value : value
+      };
+    }
+  }));
+
+//Connect-Flash Middleware
+app.use(flash());
+app.use(function(req, res, next){
+    res.locals.messages = require ('express-messages')(req ,res);
+    next();
+});
+
+app.get("*", function(req, res, next){
+  res.locals.user = req.user || null;
+  next();
+})
+
+// Define Routes
+app.use('/', routes);
+app.use('/users', users)
+
+app.listen();
+console.log('Server started on port 3000');
